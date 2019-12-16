4dcce9f95bded7163cacb02b44b17e4f437243ba
SRsajeeb <sajeebsrs@gmail.com>
2019-12-17 00:48:27 +0600

    update

--- CF gym-102397D.cpp
+++ CF gym-102397D.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-    long x,y,sum=0,c=0,s=0;
+    long x,y,sum=0,c=0,s=0,count[1005],j=0;
     cin>>x>>y;
     long arr[x];
 
@@ -12,6 +12,41 @@ int main()
         cin>>arr[i];
         sum+=arr[i];
     }
-  ///did not solve
+    ///did not solve yet
+    if(sum<y)
+        cout<<"-1"<<endl;
+    else
+    {
+        int p = 0;
+        j = 0;
+        for(int i=0; i<x; i++)
+        {
+            j+=i;
+
+            while(j<x)
+            {
+
+                s += arr[j];
+                c++;
+                j++;
+                if(s>=y)
+                {
+                    count[p] = c;
+                    p++;
+                    break;
+                }
+
+            }
+            //if(s<y)break;
+            s = 0;
+            c = 0;
+            j = 0;
+        }
+//     for(int i=0; i<p; i++)
+//        cout<<count[i]<<" ";
+//        cout<<endl;
+        sort(count,count+p);
+        cout<<count[0]<<endl;
+    }
 
 }
