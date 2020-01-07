5565c5ce2ee4f463d2c91714c085cebc6a5c6b6b
SRsajeeb <sajeebsrs@gmail.com>
2020-01-08 02:29:21 +0600

    update

--- CodeForces/CF 230B-T-primes.cpp
+++ CodeForces/CF 230B-T-primes.cpp
@@ -1,41 +1,40 @@
-#include<bits/stdc++.h> ///didnt solve yet
+#include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-vector<ll>tprime;
-bool sieve(ll n){
-   if(n>1){
-    for(int i=2; i*i<=n; i++){
-        if(n%i==0)return false;
-    }return true;
-   }return false;
-
-}
-void gen(){
-    for(ll i=1; i<1000000; i++){
-        if(sieve(i)){
-            tprime.push_back(i*i);
+bool sieve(ll t)
+{
+    ll n = sqrt(t);
+    if(n>1)
+    {
+        for(ll i=2; i*i<=n; i++)
+        {
+            if(n%i==0)
+                return false;
         }
+        return true;
     }
-
+    return false;
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    bool flag;
-    gen();
-    ll n;
+
+    ll n,tem,num;
     cin>>n;
-    ll arr[n];
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
-    }
-    for(int i=0; i<n; i++){
-        flag = binary_search(tprime.begin(),tprime.end(),arr[i]);
-        if(flag)
+    while(n--)
+    {
+        cin>>num;
+        tem = sqrt(num);
+        if(tem * tem == num && sieve(num))
+        {
             cout<<"YES"<<'\n';
+        }
         else
+        {
             cout<<"NO"<<'\n';
+        }
+
     }
 }
