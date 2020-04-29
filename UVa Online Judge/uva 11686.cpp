481d523b456faafd882b83f19a5ce59aaf50fa4c
SRsajeeb <sajeebsrs@gmail.com>
2020-04-29 06:27:43 +0600

    updated solution

--- UVa Online Judge/uva 11686.cpp
+++ UVa Online Judge/uva 11686.cpp
@@ -2,38 +2,40 @@
 using namespace std;
 stack<int>st;
 bool flag = false;
-void dfs(int u,vector<vector<int>>&graph, vector<bool>&v)
+void dfs(int u,vector<vector<int>>&graph, vector<int>&v)
 {
-    v[u] = true;
+    v[u] = 1;
     for(auto i:graph[u])
         if(!v[i])
             dfs(i,graph,v);
-        else{
-            flag = true;
+        else if(v[i]==1){
+            flag = false;
         }
+    v[u]=2;
     st.push(u);
 }
 
 int main()
 {
     int n,m;
+    //freopen("output.txt","w",stdout);
     while(cin>>n>>m,n,m)
     {
         int a,b;
         vector<vector<int>>graph(n+1);
-        vector<bool>v(n+1,false);
+        vector<int>v(n+1,0);
         for(int i=0; i<m; i++)
         {
             cin>>a>>b;
             graph[a].push_back(b);
         }
-        flag = false;
+        flag = true;
         for(int i=1; i<=n; i++)
         {
             if(!v[i])
                 dfs(i,graph,v);
         }
-        if(flag)
+        if(!flag)
         {
             cout<<"IMPOSSIBLE"<<endl;
             while(!st.empty())st.pop();
